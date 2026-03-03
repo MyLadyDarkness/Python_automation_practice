58ddb51943c3f2500607b2cdc3cbf63bbd75ba31
Darkness <Cleopathra@yandex.ru>
2026-03-03 15:15:04 +0300

    feat: add update_pet_form_data method to collect form data inside and update pet by id

--- src/api/endpoints/petstore.py
+++ src/api/endpoints/petstore.py
@@ -5,7 +5,18 @@ class PetStore(Client):
 
 #------------------------All about pet--------------------
     def create_pet(self, pet_data):
-        return self.post("/pet", json=pet_data)
+        return self.post("/pet", json = pet_data)
+
+    def update_pet_form_data(self, pet_id, name=None, status=None):
+        pet_data = {}
+
+        if name:
+            pet_data["name"] = name
+        if status:
+            pet_data["status"] = status
+
+        return self.post(f"/pet/{pet_id}", data = pet_data)
+
 
     def get_pet(self, pet_id):
         return self.get(f"/pet/{pet_id}")
