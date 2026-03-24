9eaa4da394d1573648678c0e8ca46a22c7d6eeb1
MyLadyDarkness <Cleopathra@yandex.ru>
2026-03-24 16:03:30 +0300

    WIP: add allure reports for test_login_error UI test

--- tests/saucedemo-ui/test_login.py
+++ tests/saucedemo-ui/test_login.py
@@ -1,35 +1,48 @@
-from time import sleep
-
 import pytest
 from selenium.webdriver.common.by import By
 from src.pages.saucedemo.base_page import SauceBasePage
+import allure
 
 
-@pytest.mark.parametrize("login, password, error_msg" ,
+@pytest.mark.smoke
+@allure.title("Вход в систему. НЕГАТИВНЫЙ ТСЕТ")
+@allure.feature("LOGIN")
+@allure.severity(allure.severity_level.CRITICAL)
+@pytest.mark.parametrize("login, password, error_msg",
                          [
                              ("", "", "Epic sadface: Username is required"),
                              ("s", "", "Epic sadface: Password is required"),
                              ("s", "s", "Epic sadface: Username and password do not match any user in this service"),
                              ("locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out.")
-                          ],
+                         ],
 
-                        ids=[
-                                "empty_login_and_password",
-                                "empty_password_only",
-                                "not_registered_user",
-                                "locked_out_user"
-                            ]
+                         ids=[
+                             "empty_login_and_password",
+                             "empty_password_only",
+                             "not_registered_user",
+                             "locked_out_user"
+                         ]
                          )
-
 def test_login_error(driver, login, password, error_msg):
     login_page = SauceBasePage(driver)
-    login_page.open()
-    assert  "Swag Labs" in driver.title
+
+    with allure.step("1. Открытие страницы"):
+        login_page.open()
+        assert "Swag Labs" in driver.title
+
+    screenshot = driver.get_screenshot_as_jpg()
+    allure.attach(screenshot, name="Страница открыта", attachment_type=allure.attachment_type.JPG)
 
     login_page.login(login)
     login_page.password(password)
-    login_page.click((By.ID, "login-button"))
-    login_page.assertions_error(error_msg)
+
+    with allure.step("2. Вход в систему"):
+        login_page.click((By.ID, "login-button"))
+
+    with allure.step("3. Сообщение об ошибке"):
+        login_page.assertions_error(error_msg)
+
+    allure.attach(f"Ошибка: {error_msg}", screenshot, name="Ошибка", attachment_type=allure.attachment_type.TEXT)
 
 
 @pytest.mark.parametrize("login, password",
@@ -49,7 +62,6 @@ def test_login_error(driver, login, password, error_msg):
                              "visual_user"
                          ]
                          )
-
 def test_login_success(driver, login, password):
     login_page = SauceBasePage(driver)
     login_page.open()
@@ -60,8 +72,3 @@ def test_login_success(driver, login, password):
     login_page.click((By.ID, "login-button"))
     login_page.assertions_ok()
     login_page.logout()
-
-
-
-
-
